9a2a85d2ba3207294ee97ad5cefa67d1267a9e62
tiye <jiyinyiyong@gmail.com>
2025-10-28 01:13:00 +0800

    Initial Commit

---
+++ shader-bg/shader_bgApp.swift
@@ -0,0 +1,17 @@
+//
+//  shader_bgApp.swift
+//  shader-bg
+//
+//  Created by chen on 2025/10/28.
+//
+
+import SwiftUI
+
+@main
+struct shader_bgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
